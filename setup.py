1d498b0094a64057b362da64a519b3bf523a4cb4
Pavlos Kallis <pkallis@douleutaras.com>
2022-11-12 21:42:11 +0200

    Release: v0.2.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pdmongo',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='Transfer data between pandas dataframes and MongoDB',
     long_description='%s\n%s' % (
